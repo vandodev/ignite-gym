d702175ea025c78566c300453d088c5d2bea38cf
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-08 01:31:30 -0300

    :rocket: Personalizando a statusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -8,5 +7,21 @@ import {
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  return <View>{fontsLoaded ? <Text>Hello world</Text> : <View />}</View>;
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello world</Text> : <View />}
+    </View>
+  );
 }
