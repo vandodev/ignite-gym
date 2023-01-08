5a2f0a5d54becd268b148678041d2b19c2c1a7dd
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-08 01:13:03 -0300

    :rocket: Fontes personalizadas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return <View>{fontsLoaded ? <Text>Hello world</Text> : <View />}</View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
