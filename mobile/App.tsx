44d9392711f500f3b9a1eafa4eb3d2bdf91f65e8
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-09 01:12:14 -0300

    :rocket: Criando component Loading com o nativeBase

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,23 @@
 import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from "@components/Loading";
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello world</Text> : <View />}
-    </View>
+      <Loading />
+    </NativeBaseProvider>
   );
 }
