c69438f8d7c70d04c502f400f49010b86b27763a
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-10 00:52:32 -0300

    :rocket: Componente de Input

---
+++ mobile/src/components/Input.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  FormControl,
+} from "native-base";
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      bg="gray.700"
+      h={14}
+      px={4}
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily="body"
+      mb={4}
+      placeholderTextColor="gray.300"
+      {...rest}
+    />
+  );
+}
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { VStack, Image, Center, Text, Heading } from "native-base";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
@@ -24,6 +25,8 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse a conta
         </Heading>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   );
