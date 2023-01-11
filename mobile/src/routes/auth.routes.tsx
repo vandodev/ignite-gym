ec14a029c132b484e7efa22e0e1c60992bddc24d
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-11 00:53:49 -0300

    :rocket: Tipando as rotas

--- mobile/src/routes/auth.routes.tsx
+++ mobile/src/routes/auth.routes.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 
 import { SignIn } from "@screens/SignIn";
 import { SignUp } from "@screens/SignUp";
 
-const { Navigator, Screen } = createNativeStackNavigator();
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+};
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export function AuthRoutes() {
   return (
