655798fbe27afe981fee183074a7a66deae6ffca
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-12 00:11:38 -0300

    :rocket: Criando a estruturas das interfaces do App

---
+++ mobile/src/screens/History.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Center, Text } from "native-base";
+export function History() {
+  return (
+    <Center flex={1}>
+      <Text>History</Text>
+    </Center>
+  );
+}
