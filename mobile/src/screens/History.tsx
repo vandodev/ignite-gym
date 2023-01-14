4a5ee37a061d28c56f65f4409615caaf95d98799
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-14 01:01:40 -0300

    :rocket: Componente HistoryCard

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { ScreenHeader } from "@components/ScreenHeader";
 import { VStack } from "native-base";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico" />
+      <HistoryCard />
     </VStack>
   );
 }
