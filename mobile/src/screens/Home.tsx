c16aa815477fa51556fd3cd635775f0d1ec825c7
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-14 00:25:40 -0300

    :rocket: Criando componente ExerciseCard

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import { Center, HStack, VStack, FlatList, Heading, Text } from "native-base";
+
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState("antebraço");
@@ -45,6 +47,8 @@ export function Home() {
             4
           </Text>
         </HStack>
+        <ExerciseCard />
+        <ExerciseCard />
       </VStack>
     </VStack>
   );
