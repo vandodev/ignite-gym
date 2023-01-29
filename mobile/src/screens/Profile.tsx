d3b522c4cb35e0689cc85fe3a0a7c879adc45053
Evandro oliveira <vandoaparecido@hotmail.com>
2023-01-29 00:42:39 -0300

    :rocket: Efeito Skeleton e botão alterar foto

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -1,20 +1,47 @@
-import React from "react";
-import { Center, ScrollView, VStack } from "native-base";
+import React, { useState } from "react";
+import { TouchableOpacity } from "react-native";
+import { Center, Text, ScrollView, VStack, Skeleton } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 
+const PHOTO_SIZE = 33;
+
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(true);
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/vandodev.png" }}
-            alt="Foto do usuário"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <UserPhoto
+              // source={{ uri: "https://github.com/vandodev.png" }}
+              source={{ uri: "" }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
