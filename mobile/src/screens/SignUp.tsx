533469e30a6671aa041fccc2cb7740485bd29e50
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-02 00:02:52 -0300

    :rocket: Rules Patterns

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -77,6 +77,13 @@ export function SignUp() {
           <Controller
             control={control}
             name="email"
+            rules={{
+              required: "Informe o email.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "E-mail inválido",
+              },
+            }}
             render={({ field: { onChange, value } }) => (
               <Input
                 placeholder="E-mail"
@@ -87,6 +94,7 @@ export function SignUp() {
               />
             )}
           />
+          <Text color="white">{errors.email?.message}</Text>
 
           <Controller
             control={control}
