07fac8cd4535810aa38d863eaa83b1ecd2521df2
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-03 01:28:49 -0300

    :rocket: Trabalhando com Async Await

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -46,17 +46,17 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password }: FormDataProps) {
-    fetch("http://192.168.1.5:3333/users", {
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    const response = await fetch("http://192.168.1.5:3333/users", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name, email, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
