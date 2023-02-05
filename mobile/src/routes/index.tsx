97d17114b2efdea2fff48c4cf723b05dcd91ca73
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-05 00:22:18 -0300

    :rocket: Criando seu próprio Hook

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { useTheme, Box } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
-import { AuthContext } from "@contexts/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
@@ -12,9 +12,9 @@ export function Routes() {
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
-  const contextData = useContext(AuthContext);
+  const { user } = useAuth();
 
-  console.log("USUÁRIO LOGADO =>", contextData);
+  console.log("USUÁRIO LOGADO =>", user);
 
   return (
     <Box flex={1} bg="gray.700">
