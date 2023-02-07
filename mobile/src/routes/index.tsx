f399462e29d338cc92980e7f22ceba4c760ed39c
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-07 00:39:05 -0300

    :rocket: Exibindo Loading enquanto carrega usuário

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -7,12 +7,18 @@ import { useAuth } from "@hooks/useAuth";
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 
+import { Loading } from "@components/Loading";
+
 export function Routes() {
   const { colors } = useTheme();
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
+
+  if (isLoadingUserStorageData) {
+    return <Loading />;
+  }
 
   return (
     <Box flex={1} bg="gray.700">
