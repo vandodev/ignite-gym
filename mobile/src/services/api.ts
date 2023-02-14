06a857b511370b427350c33b5ce2ff17ba379938
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-14 00:54:20 -0300

    :rocket: Refatorando, incluindo e armazenando refresh_token

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import { AppError } from "@utils/AppError";
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 type SignOut = () => void;
 
@@ -12,9 +13,15 @@ const api = axios.create({
 })as APIInstanceProps;
 
 api.registerInterceptTokenManager = singOut => {
-const interceptTokenManager = api.interceptors.response.use((response) => response, requestError => {
+const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
     if(requestError.response?.status === 401) {
       if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+          const { refresh_token } = await storageAuthTokenGet();
+
+        if(!refresh_token) {
+          singOut();
+          return Promise.reject(requestError)
+        }
 
       }
 
@@ -33,5 +40,6 @@ const interceptTokenManager = api.interceptors.response.use((response) => respon
   return () => {
     api.interceptors.response.eject(interceptTokenManager);
   }}
+  
 
 export { api };
