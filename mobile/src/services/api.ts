e1e6730e263b42bc7258bac3cc2eef89fb78154d
Evandro oliveira <vandoaparecido@hotmail.com>
2023-02-14 00:11:50 -0300

    :rocket: Incluindo gerenciador de Token na API

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -11,12 +11,17 @@ const api = axios.create({
   baseURL: 'http://192.168.1.5:3333'
 })as APIInstanceProps;
 
-api.interceptors.response.use((response) => response, (error) => {
-  if(error.response && error.response.data) {
-    return Promise.reject(new AppError(error.response.data.message))
-  } else {
-    return Promise.reject(error)
-  }
-})
+api.registerInterceptTokenManager = singOut => {
+  const interceptTokenManager = api.interceptors.response.use((response) => response, (error) => {
+    if(error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message))
+    } else {
+      return Promise.reject(error)
+    }
+  });
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  }}
 
 export { api };
